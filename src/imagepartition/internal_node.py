7d6d58a77dd78fb8538ce8496db7e9193c6d8c94
chiang67 <sean.chiang@mail.utoronto.ca>
2020-09-07 04:38:16 -0400

    Add default values to InternalNode init

--- src/imagepartition/internal_node.py
+++ src/imagepartition/internal_node.py
@@ -8,8 +8,8 @@ class InternalNode(TreeNode):
     A node of the quadtree that is a parent to some other node or None.
     """
 
-    def __init__(self, q1: Optional[TreeNode], q2: Optional[TreeNode],
-                 q3: Optional[TreeNode], q4: Optional[TreeNode]) -> None:
+    def __init__(self, q1: Optional[TreeNode] = None, q2: Optional[TreeNode] = None,
+                 q3: Optional[TreeNode] = None, q4: Optional[TreeNode] = None) -> None:
         self.quadrants = [q1, q2, q3, q4]
 
     def set_quadrant(self, index: int, node: Optional[TreeNode]) -> None:
